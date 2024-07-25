43b8dcb0ce11c8449b8988258ad3b13555a19372
guaiyoui <1748659307@qq.com>
2024-07-25 03:45:35 +0000

    add active learning

---
+++ Active_Learning/test.py
@@ -0,0 +1,29 @@
+# python run_baselines.py --dataset spammer --model GCN --epoch 300 --strategy random --file_io 1 --lr 0.01 --hidden 16
+
+import pickle as pkl
+import sys
+import numpy as np
+
+def parse_index_file(filename):
+    """Parse index file."""
+    index = []
+    for line in open(filename):
+        index.append(int(line.strip()))
+    return index
+
+names = ['x', 'y', 'tx', 'ty', 'allx', 'ally', 'graph']
+objects = []
+
+dataset_str = "cora"
+for i in range(len(names)):
+    with open("./data/ind.{}.{}".format(dataset_str.lower(), names[i]), 'rb') as f:
+        if sys.version_info > (3, 0):
+            objects.append(pkl.load(f, encoding='latin1'))
+        else:
+            objects.append(pkl.load(f))
+
+x, y, tx, ty, allx, ally, graph = tuple(objects)
+test_idx_reorder = parse_index_file("./data/ind.{}.test.index".format(dataset_str))
+test_idx_range = np.sort(test_idx_reorder)
+
+print(y)
